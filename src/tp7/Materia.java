2a67a21a03034ffe40cdbf3e40bcada8bb2bee4b
Miguel Aguero <>
2020-05-01 19:33:03 -0300

    Se modifico el UML. Se completaron los metodos de la clase materia.

--- src/tp7/Materia.java
+++ src/tp7/Materia.java
@@ -1,34 +1,24 @@
 package tp7;
 
 public class Materia {
-
     private int idMateria;
-
     private String nombre;
-
     private int anio;
 
     public Materia(int idMateria, String nombre, int anio) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
     }
 
-    public int getIdMateria() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public int getIdMateria() { return idMateria; }
+    public void setIdMateria(int idMateria) { this.idMateria = idMateria; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public int getAnio() { return anio; }
+    public void setAnio(int anio) { this.anio = anio;
     }
 
-    public void setIdMateria(int materia) {
-    }
-
-    public String getNombre() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public void setNombre(String nombre) {
-    }
-
-    public int getAnio() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public void setAnio(int anio) {
-    }
 }
