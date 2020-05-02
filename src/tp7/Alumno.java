9f6c24d49ba7d45746279f5a087a786f19b59225
Miguel Aguero <HP@miguel-PC>
2020-05-01 21:13:39 -0300

    Se modificó el modelo UML. Se agregó el método listarMaterias a la clase Alumno y se modifico la visibilidad del método cantidadMaterias.

--- src/tp7/Alumno.java
+++ src/tp7/Alumno.java
@@ -1,6 +1,7 @@
 package tp7;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Alumno {
 
@@ -24,11 +25,32 @@ public class Alumno {
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
 
+    /**
+     * Este método recibe por parametro una materia y la agrega a la lista de materias inscriptas del alumno, 
+     * siempre y cuando la materia no figure en la su lista. Esta funcionalidad 
+     * está implementada por el tipo de estructura de datos utilizada (HashSet).
+     * @param m 
+     */
     public void agrgarMateria(Materia m) {
-        misMaterias.add(m);
+        if ( misMaterias.add(m) ) {
+            System.out.println(nombre + " se inscribió en: " + m.getNombre());
+        } else {
+            System.out.println(nombre + " ya está inscripto/a en: " + m.getNombre());
+        }
     }
 
-    public int cantidadMaterias() {
+    private int cantidadMaterias() {
         return misMaterias.size();
+    }
+    
+    public void listarMaterias(){
+        System.out.println("\nAlumno/a: " + nombre + " " + apellido);
+        System.out.println("Cantidad de materias en las que esta inscripto/a: " +  cantidadMaterias());
+        System.out.println("[ idMateria ][ nombre ][ anio ]");
+        Iterator it = misMaterias.iterator();
+        while (it.hasNext()) {
+            Materia mat = (Materia)it.next();
+            System.out.println("[ " + mat.getIdMateria() + " ][ " + mat.getNombre() + " ][ " + mat.getAnio() + " ]");
+        }
     }
 }
--- src/tp7/Colegio.java
+++ src/tp7/Colegio.java
@@ -4,23 +4,28 @@ public class Colegio {
 
     public static void main(String[] args) {
         
+        //a) Crear las materias:
         Materia mat1 = new Materia(1,"Ingles I",1);
         Materia mat2 = new Materia(2,"Matematicas",1);
         Materia mat3 = new Materia(3,"Laboratorio I",1);
         
+        //b) Crear 2 alumnos.
         Alumno alu1 = new Alumno(1001, "López", "Martin");
         Alumno alu2 = new Alumno(1002, "Martínez", "Brenda");
         
+        //c) Inscribir a López en las 3 materias.
         alu1.agrgarMateria(mat1);
         alu1.agrgarMateria(mat2);
         alu1.agrgarMateria(mat3);
 
+        //d) Inscribir a Martínez en las 3 materias y volver a inscribirlo en Laboratorio 1.
         alu2.agrgarMateria(mat1);
         alu2.agrgarMateria(mat2);
         alu2.agrgarMateria(mat3);
         alu2.agrgarMateria(mat3);
         
-        System.out.println("Cantidad de materias de " + alu1.getApellido() + " - " + alu1.cantidadMaterias());
-        System.out.println("Cantidad de materias de " + alu2.getApellido() + " - " + alu2.cantidadMaterias());
+        alu1.listarMaterias();
+        alu2.listarMaterias();
+    
     }
 }
